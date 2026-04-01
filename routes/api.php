74edfff6ec32a478fdc0f3b3405656465c7ae54d
Averroes098 <muhammadnafi.2023@student.uny.ac.id>
2026-04-01 22:57:18 +0700

    Initial commit atau pesan Anda

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SiswaController;
 use App\Http\Controllers\Api\KelasController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API jalan']);
@@ -10,3 +11,5 @@ Route::get('/test', function () {
 
 Route::apiResource('siswa', SiswaController::class);
 Route::apiResource('kelas', KelasController::class);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
